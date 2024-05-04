8ed20353436e581a8421883c777a2966ef3fcb93
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2024-05-04 16:34:33 +0200

    put utils in folder

--- train.py
+++ train.py
@@ -6,10 +6,10 @@ import torch
 import argparse
 import torch.nn as nn
 from module import PETER
-from utils import DataLoader, Batchify, now_time
+from utils.peter_utils import DataLoader, Batchify, now_time
 
 
-from utils import move_content_to_device, peter_print_long, peter_content, peter_loss_good # les q he afegit jo
+from utils.andreu_utils import move_content_to_device, peter_print_long, peter_content, peter_loss_good # les q he afegit jo
 from test import test
 
 
